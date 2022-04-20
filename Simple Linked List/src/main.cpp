c8a3d183e0c703499cbf41b977b35ebbfe29053e
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2022-04-20 16:05:14 -0300

    Fix comment

--- Simple Linked List/src/main.cpp
+++ Simple Linked List/src/main.cpp
@@ -10,7 +10,7 @@ int main()
 	list.insertTail(4); // 1, 2, 3, 4
 	list.insertHead(6); // 6, 1, 2, 3, 4
 	list.insertHead(7); // 7, 6, 1, 2, 3, 4
-	list.remove(1);		// 7, 6, 2, 3, 4
+	list.remove(1);     // 7, 6, 2, 3, 4
 	list.insertTail(2); // 7, 6, 2, 3, 4, 2
 	list.remove(5);     // 7, 6, 2, 3, 4, 2
 	 // 7, 6, 2, 3, 4, 2
